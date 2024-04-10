c90cb48401eafabba95bdd9c4201661970ac4f43
pavel_pokidko <pp.market-sletat@yandex.ru>
2024-04-10 17:09:00 +0300

    поправил JSON генератор

--- json_generate.py
+++ json_generate.py
@@ -7,5 +7,25 @@ cursor = connection.cursor()
 cursor.execute("SELECT * FROM Users")
 
 table = cursor.fetchall()
+head = [
+    'user_id',
+    'username',
+    'date',
+    'name',
+    'depart_city',
+    'resort',
+    'quan',
+    'dates',
+    'nights',
+    'budget',
+    'messanger',
+    'number'
+]
+table_dict = []
+k = 0
+for i in table:
+    dictionary = dict(zip(head, table[k]))
+    table_dict.append(dictionary)
+    k += 1
 with open('users.json', mode='w', buffering=-1, encoding='utf-8') as f:
     json.dump(table, f, indent=2)
